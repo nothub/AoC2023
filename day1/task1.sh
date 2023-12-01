a16278b981b91288ad71c3a4eadd8f43342fc6b0
Florian Hübner <code@hub.lol>
2023-12-01 15:56:00 +0100

    day 1

---
+++ day1/task1.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+result=0
+
+while IFS= read -r line; do
+    digits="$(echo "${line}" | tr -d '[:alpha:]')"
+    summand="$(echo "$digits" | cut -c 1)$(echo "$digits" | rev | cut -c 1)"
+    result=$((result + summand))
+done <"input.txt"
+
+echo $result
